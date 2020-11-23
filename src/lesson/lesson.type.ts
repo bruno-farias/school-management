22a24f82843ca796d6f61b55b67c2eba72473653
Bruno Farias <brunofarias1@gmail.com>
2020-11-23 22:33:21 +0200

    created mutation to persist a lesson

--- src/lesson/lesson.type.ts
+++ src/lesson/lesson.type.ts
@@ -2,15 +2,15 @@ import {Field, ID, ObjectType} from "@nestjs/graphql";
 
 @ObjectType('Lesson')
 export class LessonType {
-    @Field(type => ID)
-    id: string;
+  @Field(type => ID)
+  id: string;
 
-    @Field()
-    name: string;
+  @Field()
+  name: string;
 
-    @Field()
-    startDate: string;
+  @Field()
+  startDate: string;
 
-    @Field()
-    endDate: string;
+  @Field()
+  endDate: string;
 }
